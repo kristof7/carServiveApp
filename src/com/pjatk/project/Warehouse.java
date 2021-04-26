f3eb664b54fea77fa30df7a1f91df42b7be582d5
Krzysztof Kampa <59344972+kristof7@users.noreply.github.com>
2021-04-26 19:33:18 +0200

    Warehouse class update

--- src/com/pjatk/project/Warehouse.java
+++ src/com/pjatk/project/Warehouse.java
@@ -5,7 +5,17 @@ import java.util.List;
 public class Warehouse {
 
     private int percentageServiceArea;
-    List<Person> persons;
+    public List<Person> persons;
 
-    private Person payer = persons.get(0);
+    private Person payer;
+
+    public Warehouse(int percentageServiceArea, List<Person> persons) {
+        this.percentageServiceArea = percentageServiceArea;
+        this.persons = persons;
+        if (persons != null) {
+            this.payer = persons.get(0);
+        } else {
+            this.payer = null;
+        }
+    }
 }
